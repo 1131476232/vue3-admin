4ec91075bf0967b384b6715a6d516c97e0fc20b0
张林 <rememberflyfly@163.com>
2026-03-29 10:05:16 +0800

    fix: 修改配置

--- .history/vite.config_20260329094336.js
+++ .history/vite.config_20260329094336.js
@@ -6,20 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  // 关键：适配 Cloudflare 部署
-  server: {
-    port: 3000,
-    open: false
-  },
-  build: {
-    outDir: 'dist',
-    minify: 'esbuild'
-  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
-
